b017b788344a21fa0b3f98b239434cd7e7765d07
TolikKorobskix <ggamer556@mail.ru>
2021-12-07 15:26:13 +0300

    v1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from './components/Calendar';
 
 function App() {
+  const [date, setDate] = useState(null)
+
+  const handleChangeDate = date => setDate(date);
+
   return (
     <div>
-     
+      {date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
+     <Calendar
+       onChange={handleChangeDate}
+     />
     </div>
   );
 }
